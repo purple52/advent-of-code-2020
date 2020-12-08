0145acfdf3ceca991942aa06d44df72be79b09ba
David Edwards <david@more.fool.me.uk>
2020-12-08 09:52:32 +0000

    Tidy up day 7 slightly

--- day7/day7-part1.py
+++ day7/day7-part1.py
@@ -6,10 +6,10 @@ def parse(line):
     return match('^[a-z]+ [a-z]+', line).group(), findall('\\d+ ([a-z]+ [a-z]+)', line)
 
 
-def count_contains(item, rules):
+def contains(item, rules):
     def find(r): return item in rules[r] or any(map(find, rules[r]))
 
-    return sum(map(find, rules))
+    return map(find, rules)
 
 
-print(count_contains('shiny gold', dict(map(parse, open('input/actual.txt').read().splitlines()))))
+print(sum(contains('shiny gold', dict(map(parse, open('input/actual.txt').read().splitlines())))))
