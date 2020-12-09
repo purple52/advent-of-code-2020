0cc53bf04fe26ea018568fc23528fd1cd277a733
David Edwards <david@more.fool.me.uk>
2020-12-09 06:44:32 +0000

    Clearer solution for part 2 of day 8

--- day8/day8-part2.py
+++ day8/day8-part2.py
@@ -1,8 +1,20 @@
-def execute(p, i=0, v=frozenset(), a=0, m=0):
-    return None if m > 1 or i in v else a if i == len(p) else {
-        'acc': lambda: execute(p, i + 1, v.union({i}), a + p[i][1], m),
-        'jmp': lambda: execute(p, i + p[i][1], v.union({i}), a, m) or execute(p, i + 1, v.union({i}), a, m + 1),
-        'nop': lambda: execute(p, i + 1, v.union({i}), a, m) or execute(p, i + p[i][1], v.union({i}), a, m + 1)
+def acc(p, i, v, a, m):
+    return execute(p, i + 1, v.union({i}), a + p[i][1], m)
+
+
+def jmp(p, i, v, a, m):
+    return execute(p, i + p[i][1], v.union({i}), a, m)
+
+
+def nop(p, i, v, a, m):
+    return execute(p, i + 1, v.union({i}), a, m)
+
+
+def execute(p, i=0, v=frozenset(), a=0, m=True):
+    return None if i in v else a if i == len(p) else {
+        'acc': lambda: acc(p, i, v, a, m),
+        'jmp': lambda: jmp(p, i, v, a, m) or (nop(p, i, v, a, False) if m else None),
+        'nop': lambda: nop(p, i, v, a, m) or (jmp(p, i, v, a, False) if m else None)
     }.get(p[i][0])()
 
 
